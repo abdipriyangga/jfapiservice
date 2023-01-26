d5fab992ab0ced413356eeebfd16ad10ed5eb5f2
abdipriyangga <mabdipme@gmail.com>
2023-01-26 14:39:39 +0700

    add allowed header properti in cors

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const port = process.env.PORT;
-const mainRouter = require('./src/routes/index');
-const cors = require('cors');
+const mainRouter = require("./src/routes/index");
+const cors = require("cors");
 
-app.use(cors({
-  origin: ["http://localhost:6969", "http://localhost:3000", "https://jannahfirdaus.com", "https://nest.jannahfirdaus.com", "https://service.jannahfirdaus.com"],
-  credentials: true,
-  allowedHeaders: []
-}));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:6969",
+      "http://localhost:3000",
+      "https://jannahfirdaus.com",
+      "https://nest.jannahfirdaus.com",
+      "https://service.jannahfirdaus.com",
+    ],
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(process.env.APP_UPLOAD_ROUTE, express.static('public/pictures'))
+app.use(process.env.APP_UPLOAD_ROUTE, express.static("public/pictures"));
 app.listen(port, () => {
-  console.log(`App running on port ${port}.`)
-})
+  console.log(`App running on port ${port}.`);
+});
 
-app.use('/', mainRouter);
+app.use("/", mainRouter);
 module.exports = app;
